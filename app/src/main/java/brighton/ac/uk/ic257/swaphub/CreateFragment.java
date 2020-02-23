0fb53d83c6f0fb55403183c40a006ccca5c90a92
Iuliia <iuliiachoporova@gmail.com>
2020-02-23 23:42:55 +0000

    image picker added

--- app/src/main/java/brighton/ac/uk/ic257/swaphub/CreateFragment.java
+++ app/src/main/java/brighton/ac/uk/ic257/swaphub/CreateFragment.java
@@ -1,5 +1,7 @@
 package brighton.ac.uk.ic257.swaphub;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -7,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,9 +19,12 @@ import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import static android.app.Activity.RESULT_OK;
 
 public class CreateFragment extends Fragment {
-
+    private static final int PICK_IMAGE_REQUEST = 1;
     EditText editTextName;
     EditText editTextDescription;
     EditText editTextSwapFor;
@@ -25,6 +32,12 @@ public class CreateFragment extends Fragment {
     EditText editTextUserPhone;
     Spinner spinnerCategory;
     Button buttonAdd;
+
+
+    Button buttonChoosePhoto;
+    ImageView imageView;
+    ProgressBar progressBar;
+    Uri mImageUri;
     DatabaseReference databaseItems;
     public CreateFragment(){
         // empty constructor
@@ -46,6 +59,17 @@ public class CreateFragment extends Fragment {
         editTextUserPhone = view.findViewById(R.id.editTextUserPhone);
         spinnerCategory = view.findViewById(R.id.spinner);
         buttonAdd = view.findViewById(R.id.buttonAdd);
+
+        buttonChoosePhoto = view.findViewById(R.id.buttonChoosePhoto);
+        imageView = view.findViewById(R.id.imageView);
+        progressBar = view.findViewById(R.id.progressBar);
+
+        buttonChoosePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
 
         databaseItems = FirebaseDatabase.getInstance().getReference("Items");
         buttonAdd.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +98,23 @@ public class CreateFragment extends Fragment {
         }
         else{
             Toast.makeText(getActivity(), "Enter Name", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+
+            Picasso.get().load(mImageUri).into(imageView);
         }
     }
 }
