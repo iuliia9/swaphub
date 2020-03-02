9c9d224b2ee7d316320ff7594a13e491173a03f3
Iuliia <iuliiachoporova@gmail.com>
2020-03-02 15:37:53 +0000

    Added a top app bar

--- app/src/main/java/brighton/ac/uk/ic257/swaphub/HomeActivity.java
+++ app/src/main/java/brighton/ac/uk/ic257/swaphub/HomeActivity.java
@@ -6,10 +6,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -25,20 +30,46 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btnLogout = findViewById(R.id.logout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(HomeActivity.this, MainActivity.class));
-            }
-        });
+//        btnLogout = findViewById(R.id.logout);
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+//            }
+//        });
 
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(this);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+//        getSupportActionBar().setTitle("App Title");
         //loading the default fragment
         loadFragment(new HomeFragment());
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.topappbar_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+            case R.id.action_logout:
+                Toast.makeText(this,"You are logged out", Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -55,6 +86,9 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
 
             case R.id.navigation_myswaps:
                 fragment = new MyswapsFragment();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
                 break;
         }
 
